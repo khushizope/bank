99a8cba4e8ce409986df76a9ad2a801d512e27dd
khushizope <76126723+khushizope@users.noreply.github.com>
2022-11-16 12:32:48 +0530

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,40 +1,25 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.6.0;
-contract MyBank
-{
-  mapping(address=> uint ) private _balances;
-  address public owner;
-  event LogDepositeMade(address accountHoder, uint amount );
+// SPDX-License-Identifier: MIT
+pragma solidity^0.6.0;
 
-  constructor () public
-  {
-  owner=msg.sender;
-  emit LogDepositeMade(msg.sender, 1000);
-  }
+contract Bank{
+    mapping(address=>uint) private balances;
+    address public owner;
 
-  function deposite() public payable returns (uint)
-  {
-  require ((_balances[msg.sender] + msg.value) > _balances[msg.sender] &&
-  msg.sender!=address(0));
-  _balances[msg.sender] += msg.value;
-  emit LogDepositeMade(msg.sender , msg.value);
-  return _balances[msg.sender];
+    constructor() public{
+        owner=msg.sender;
+    }
 
+    function deposit() public payable returns (uint256){
+       return balances[msg.sender] +=msg.value;
+    }
 
-  }
-  function withdraw (uint withdrawAmount) public returns (uint)
-  {
-  require (_balances[msg.sender] >= withdrawAmount);
-  require(msg.sender!=address(0));
-  require (_balances[msg.sender] > 0);
-  _balances[msg.sender]-= withdrawAmount;
-  msg.sender.transfer(withdrawAmount);
-  emit LogDepositeMade(msg.sender , withdrawAmount);
-  return _balances[msg.sender];
-  }
-
-  function viewBalance() public view returns (uint)
-  {
-  return _balances[msg.sender];
-  }
- }
+    function withdraw(uint amount) public payable returns (uint256){
+        require(balances[msg.sender]>=amount,"insufficinet balance");
+        msg.sender.transfer(amount);
+        return balances[msg.sender] -= amount;
+    }
+    function viewBalance() public view returns (uint256) {
+        
+        return balances[msg.sender];
+    }
+}
